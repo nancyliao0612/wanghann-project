fba59e8fccb047cca52e6bcf37e3fddd5f9f2148
Nancy <nancy62924@gmail.com>
2022-11-22 23:52:58 +0800

    feat: add product feature section

---
+++ client/src/components/productFeat/ProductFeat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductFeat = () => {
+  return (
+    <section class="container">
+      <h1>產品特色</h1>
+      <h2>同步分析基因+RNA+蛋白質，提升整體治療選項獲益率</h2>
+    </section>
+  );
+};
+
+export default ProductFeat;
