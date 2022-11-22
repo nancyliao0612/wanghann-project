27e9abe2dbb248144afa0584f8e8c42ad8b534f3
Nancy <nancy62924@gmail.com>
2022-11-22 23:45:54 +0800

    feat: add App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Competitor from "./components/competitors/Competitor";
+import GeneticTest from "./components/geneticTest/GeneticTest";
+import ProductFeat from "./components/productFeat/ProductFeat";
+import ProductIntro from "./components/productIntro/ProductIntro";
 
 function App() {
-  return <div className="App">Wang Hann</div>;
+  return (
+    <div className="App">
+      Wang Hann
+      <ProductIntro />
+      <GeneticTest />
+      <Competitor />
+      <ProductFeat />
+    </div>
+  );
 }
 
 export default App;
