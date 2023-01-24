7c602a5d3f0cf55c5b1a88bdf76ccf401b3de5c6
Nancy <nancy62924@gmail.com>
2023-01-24 20:31:45 +0800

    feat: add carousel fade animation

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,21 +1,56 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { HighlightText } from "../../utils";
 import carousel_one from "./images/carousel_one.png";
+import carousel_two from "./images/carousel_two.png";
+import carousel_three from "./images/carousel_three.png";
+import carousel_four from "./images/carousel_four.png";
+import carousel_five from "./images/carousel_five.png";
 import large_carousel_one from "./images/large_carousel_one.png";
+import large_carousel_two from "./images/large_carousel_two.png";
+import large_carousel_three from "./images/large_carousel_three.png";
+import large_carousel_four from "./images/large_carousel_four.png";
+import large_carousel_five from "./images/large_carousel_five.png";
 import hexagon from "../navbar/images/hexagon.svg";
+
+const dissolve = keyframes`
+0%, 30%, 100% {opacity: 0}
+5%, 25% {opacity: 1};
+`;
 
 const Carousel = styled.div`
   /* border: solid 1px green; */
-  height: 500px;
+  height: 515px;
   /* overflow: hidden; */
 
   .dissolve {
+    position: relative;
   }
 
   .items {
     height: 100%;
     position: relative;
     /* border: solid 1px orange; */
+  }
+
+  .item {
+    position: absolute;
+    left: 0;
+    right: 0;
+    opacity: 0;
+    animation: ${dissolve} 20s linear infinite;
+
+    &:nth-child(2) {
+      animation-delay: 4s;
+    }
+    &:nth-child(3) {
+      animation-delay: 8s;
+    }
+    &:nth-child(4) {
+      animation-delay: 12s;
+    }
+    &:nth-child(5) {
+      animation-delay: 16s;
+    }
   }
 
   .items > img {
@@ -35,7 +70,7 @@ const Carousel = styled.div`
   }
 
   @media screen and (min-width: 648px) {
-    height: 950px;
+    height: 1050px;
   }
 `;
 
@@ -44,6 +79,9 @@ const Text = styled.div`
   margin: 0 auto;
   margin-top: -220px;
 
+  h1 {
+    margin-bottom: unset;
+  }
   h3 {
     color: var(--color-primary);
     margin: 1.2rem 0;
@@ -73,12 +111,12 @@ const Wrapper = styled.section`
     .large {
       display: inherit;
       position: relative;
-      top: -1020px;
+      top: -1080px;
       left: 40px;
     }
     .title {
       margin: 0 1.2rem;
-      margin-top: -138rem;
+      margin-top: -150rem;
     }
   }
 `;
@@ -90,21 +128,19 @@ const Header = () => {
       <Carousel className="small carousel dissolve">
         <div className="small items">
           <img className="small item" src={carousel_one} alt="" />
-          {/* <img
-          className="item"
-          src={carousel_two}
-          alt="Seven to ten at Leeds &amp; Holbeck"
-        /> */}
+          <img className="small item" src={carousel_two} alt="" />
+          <img className="small item" src={carousel_three} alt="" />
+          <img className="small item" src={carousel_four} alt="" />
+          <img className="small item" src={carousel_five} alt="" />
         </div>
       </Carousel>
       <Carousel className="large carousel dissolve">
         <div className="large items">
           <img className="large item" src={large_carousel_one} alt="" />
-          {/* <img
-          className="item"
-          src={carousel_two}
-          alt="Seven to ten at Leeds &amp; Holbeck"
-        /> */}
+          <img className="large item" src={large_carousel_two} alt="" />
+          <img className="large item" src={large_carousel_three} alt="" />
+          <img className="large item" src={large_carousel_four} alt="" />
+          <img className="large item" src={large_carousel_five} alt="" />
         </div>
       </Carousel>
       <div className="title">
@@ -112,11 +148,7 @@ const Header = () => {
           <h1>
             OncoDNA
             <br />
-            <HighlightText>
-              業界唯一深度腫瘤分析
-              <br />
-              控制病情、恢復正常生活
-            </HighlightText>
+            <HighlightText>業界唯一深度腫瘤分析</HighlightText>
           </h1>
           <h3>
             檢測腫瘤微環境內的DNA基因組、轉錄RNA和轉譯蛋白質，能更深度解析所面臨的敵手
